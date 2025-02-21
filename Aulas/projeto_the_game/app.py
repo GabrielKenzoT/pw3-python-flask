a783950782b45b2e3b92904de7f5bc717f9a7349
GabrielKenzoT <gabrieltakeuchi418@gmail.com>
2025-02-21 11:45:42 -0300

    Terceira aula, adicionada paginas html

--- Aulas/projeto_the_game/app.py
+++ Aulas/projeto_the_game/app.py
@@ -1,21 +1,32 @@
 # Comentário em Python
 # Importando o pacote do Flask
-from flask import Flask
+from flask import Flask, render_template
 
 # Carregando o Flask na variável app
-app = Flask(__name__)
+app = Flask(__name__, template_folder='views')
 # Criando a rota principal do site
 @app.route('/')
 # Criando função no Python
-
+# View funcition - unção de visualização
 def home():
-    return '<h1>Gosto de comer</h1>'
+    return render_template('index.html')
 
 
 @app.route('/games')
 
 def games():
-    return '<ha1>seja bem vindo a página de games</h1>'
+    titulo = 'CS-GO'
+    ano = 2012
+    categoria = 'FPS online'
+    jogadores = ['iruah', 'davi_lambari', 'edosonhgf', 'kioto', 
+                 'black.butterfly', 'jujudopix']
+    jogos = ["gta 5", "lol", "Fortnite", "Elder ring", "Rust", "DayZ", "Dead by daylight"]
+    return render_template('games.html', 
+                           titulo= titulo, 
+                           ano = ano, 
+                           categoria = categoria,
+                           jogadores = jogadores,
+                           jogos = jogos)
 
 
 if __name__ == '__main__':
